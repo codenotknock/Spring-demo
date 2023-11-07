7b5acb7661cc2717d724fc8df9ce5a3d9b7afb75
xiaofu <xiaofu@dtstack.com>
2023-11-07 18:54:18 +0800

    [feat_1.0_spring_xiaofu] Spring-Cglib动态代理

---
+++ src/main/java/org/codenotknock/test/CglibProxyTest.java
@@ -0,0 +1,21 @@
+package org.codenotknock.test;
+
+import org.codenotknock.domain.Diner;
+import org.codenotknock.proxy.CglibProxy;
+
+import java.util.List;
+
+public class CglibProxyTest {
+  public static void main(String [] args){
+        // 目标对象
+        Diner diner = new Diner();
+        System.out.println(diner.getClass());
+        // 代理对象
+        Diner proxy = (Diner) new CglibProxy().getProxy(diner);
+        System.out.println(proxy.getClass());
+
+        proxy.mealToSell();
+        while (true) {}
+    }
+            
+}
