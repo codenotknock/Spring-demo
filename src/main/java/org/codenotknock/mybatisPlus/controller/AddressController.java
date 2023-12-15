3f16310bf541ac10ddfe18c4940c166716c3c4c4
xiaofu <xiaofu@dtstack.com>
2023-12-15 09:33:19 +0800

    [feat_1.0_mybatisPlus_xiaofu] [mybatisPlus知识的一套组合拳，能解决90%]

---
+++ src/main/java/org/codenotknock/mybatisPlus/controller/AddressController.java
@@ -0,0 +1,20 @@
+package org.codenotknock.mybatisPlus.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author xiaofu
+ * @since 2023-12-14
+ */
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+}
