1936f131cf94ebf78de51d2681ca0880a9729d75
xiaofu <xiaofu@dtstack.com>
2023-12-19 00:06:44 +0800

    [feat_1.5_springSecurity_xiaofu] [springSecurity源码环境准备]

---
+++ src/main/java/org/codenotknock/secycode/SecurityApplication.java
@@ -0,0 +1,15 @@
+package org.codenotknock.secycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author xiaofu
+ * springSecurity 源码的学习
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
