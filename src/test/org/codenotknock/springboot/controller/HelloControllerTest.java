a65355b246a1f856a1a975b6917e2072fde13423
xiaofu <xiaofu@dtstack.com>
2023-12-21 02:37:00 +0800

    [feat_1.0_springBoot_xiaofu]　web开发：静态资源的存放及访问

--- src/test/org/codenotknock/springboot/controller/HelloControllerTest.java
+++ src/test/org/codenotknock/springboot/controller/HelloControllerTest.java
@@ -1,7 +1,10 @@
 package org.codenotknock.springboot.controller;
 
 
+import org.apache.ibatis.annotations.Mapper;
+import org.codenotknock.springboot.mapper.StudentMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,12 @@ class HelloControllerTest {
     @Test
     public void test1() {
         System.out.println("1234...  ");
+    }
+
+    @Autowired
+    private StudentMapper studentMapper;
+    @Test
+    public void findAllTest() {
+        System.out.println(studentMapper.findAll());
     }
 }
